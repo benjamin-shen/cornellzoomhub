628178d5e8d53fcb901eb68dcb23b3cbbb1d2a4f
Benjamin Shen <bfs45@cornell.edu>
2020-08-26 17:41:27 -0400

    Init react app with helmet, router, directory structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Helmet } from "react-helmet";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import "./styles/App.css";
+
+const PageNotFound = () => {
+  return <Redirect to="/" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Cornell Zoom Hub</title>
+        <meta name="title" content="Cornell Zoom Hub" />
+        <meta
+          name="description"
+          content="Hack Our Campus 2020 - Cornell Zoom Hub"
+        />
+      </Helmet>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+import "../styles/Home.css";
+
+function Home() {
+  return <div className="home"></div>;
+}
+
+export default Home;
